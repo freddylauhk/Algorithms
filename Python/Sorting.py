c762caf4d8b575032b9c7c93e56c8cfd521319f4
Freddy <freddylau1997@gmail.com>
2019-11-06 23:33:42 +0800

    Added Comments in Sorting Programme

--- Python/Sorting.py
+++ Python/Sorting.py
@@ -1,6 +1,9 @@
+# Import random library
 from random import seed
 from random import randint
 
+# Sorting Algorithm
+# Bubble Sort
 def Bubble_Sort(arr):
     for i in range(len(arr) - 1):
         for j in range(len(arr)-1):
@@ -8,23 +11,27 @@ def Bubble_Sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr          
 
+# Generate Integer Array
 def Generate_Int_Array(n):
     arr = []
     for i in range(n):
         arr.append(randint(1, 999))
     return arr
 
+# Display Array
 def Print_Array(arr):
     print('[ ', end = '')
     for i in arr:
         print(i, end = ', ')
     print(']')
-    
-#array = [6, 5, 9, 7, 5, 3, 4, 6]
-array = Generate_Int_Array(50)
-Print_Array(array)
 
-print("Sorted Array")
+# Code Testing
+def main():
+    #array = [6, 5, 9, 7, 5, 3, 4, 6]
+    array = Generate_Int_Array(50)
+    Print_Array(array)
 
-sorted_array = Bubble_Sort(array)
-Print_Array(sorted_array)
+    print("Sorted Array")
+
+    sorted_array = Bubble_Sort(array)
+    Print_Array(sorted_array)
